ac677cdcbbb1bc43bbde7eaaf5436fd8fde86cf2
padhyar <sabihsaleh@yahoo.com>
2021-12-02 15:14:17 +0000

    Add Greeting Class

---
+++ src/main/java/com/restful/webservicres/sabih/restservice/Greeting.java
@@ -0,0 +1,19 @@
+package com.restful.webservicres.sabih.restservice;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
